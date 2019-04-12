b268c1f88e512af1bdf3a7845cabaded004ed0a0
heartofrainbow <heartofrainbow2018@gmail.com>
2019-04-12 19:39:31 +0800

    Implemented Random Waypoint Model

---
+++ random_waypoint_node.cpp
@@ -0,0 +1,6 @@
+#include "random_waypoint_node.h"
+
+random_waypoint_node::random_waypoint_node()
+{
+
+}
---
+++ random_waypoint_node.h
@@ -0,0 +1,12 @@
+#ifndef RANDOM_WAYPOINT_NODE_H
+#define RANDOM_WAYPOINT_NODE_H
+
+#include <QObject>
+
+class random_waypoint_node : public node
+{
+public:
+    random_waypoint_node();
+};
+
+#endif // RANDOM_WAYPOINT_NODE_H
